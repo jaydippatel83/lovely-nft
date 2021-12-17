4a115b0d10b9f54c0bc4615013262a4d9da54252
jaydippatel83 <jaydippatel83.pj@gmail.com>
2021-12-17 15:01:20 +0530

    Add Contracts

--- NFT.sol
+++ NFT.sol
@@ -11,7 +11,7 @@ contract NFT is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     address contractAddress;
 
-    constructor(address marketplaceAddress) ERC721("CodeCrunch", "CCT") {
+    constructor(address marketplaceAddress) ERC721("LoavelyNftToken", "LNT"){
         contractAddress = marketplaceAddress;
     }
 
